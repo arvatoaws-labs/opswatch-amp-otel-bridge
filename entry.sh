0d09026ec141c87f0ab31e47944fb2044506bb9a
Patrick Robinson <patrick.robinson@bertelsmann.de>
2022-03-24 16:07:30 +0100

    Debug
    Signed-off-by: Patrick Robinson <patrick.robinson@bertelsmann.de>

--- entry.sh
+++ entry.sh
@@ -4,4 +4,6 @@ echo "Starting Otel"
 
 cat /sample-otel-config.yaml | envsubst > /generated-otel-config.yaml
 
+cat /generated-otel-config.yaml
+
 /opt/aws/aws-otel-collector/bin/aws-otel-collector --config=/generated-otel-config.yaml
